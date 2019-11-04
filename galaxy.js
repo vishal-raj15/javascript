d1cbf1dfea7bad51419dc821578cb4e8cbb14738
vishalraj@github <50707709+vishal-raj15@users.noreply.github.com>
2019-11-05 00:34:51 +0530

    third commit

--- galaxy.js
+++ galaxy.js
@@ -2,6 +2,7 @@ var canvas = document.querySelector('#canvas');
 
 var c = canvas.getContext('2d');
 
+
 canvas.height = window.innerHeight;
 
 canvas.width = window.innerWidth;
@@ -15,7 +16,7 @@ function particle(x, y, radius, rate, color) {
     this.rate = rate;
 
     this.radian = Math.random() * 2 * Math.PI;
-    this.velocity = 0.01;
+    this.velocity = 0.008;
 
     this.update = () => {
 
@@ -35,15 +36,27 @@ function particle(x, y, radius, rate, color) {
     };
 }
 
+
+
 let particles;
+
 
 function init() {
     particles = []
     particles.push(new particle(canvas.width / 2,
-        canvas.height / 2, 7, 0, 'red'));
+        canvas.height / 2, 15, 0, 'white'));
     for (let i = 1; i <= 1000; i++) {
         particles.push(new particle(canvas.width / 2,
-            canvas.height / 2, 3.2, 0.002 * i, 'white'));
+            canvas.height / 2, 1.8 - i / 1000, 0.003 * i, 'white'));
+    }
+    for (let i = 100; i <= 1000; i++) {
+        particles.push(new particle(canvas.width / 2,
+            canvas.height / 2, 1.5, 0.002 * i, 'white'));
+    }
+
+    for (let i = 1; i <= 1000; i++) {
+        particles.push(new particle(canvas.width / 2,
+            canvas.height / 2, 2, 0.001 * i, 'white'));
     }
     // console.log(particles);
 }
@@ -64,62 +77,4 @@ animate();
 
 
 
-// var canvas = document.querySelector('#canvas');
 
-// var c = canvas.getContext('2d');
-
-// canvas.height = window.innerHeight;
-
-// canvas.width = window.innerWidth;
-
-
-// function particle(x, y, radius, rate, color) {
-//     this.x = x;
-//     this.y = y;
-//     this.radius = radius;
-//     this.color = color;
-//     this.rate = rate;
-//     this.radian = Math.PI;
-//     this.velocity = 0.05;
-
-//     this.update = () => {
-
-//         this.radian += this.velocity * rate;
-//         this.y = y * 0.4 + Math.sin(this.radian) * 100 * rate;
-//         this.x = x * 0.4 + Math.cos(this.radian) * 300 * rate;
-//         this.draw();
-//     };
-
-//     this.draw = () => {
-//         c.beginPath();
-//         c.arc(this.x, this.y, this.radius, 0,
-//             Math.PI * 2, false);
-//         c.fillStyle = this.color;
-//         c.fill();
-//         c.closePath();
-//     };
-// }
-
-// let particles;
-
-// function init() {
-//     particles = []
-//     for (let i = 1; i <= 5; i++) {
-//         particles.push(new particle(Math.random() * canvas.width / 0.5,
-//             Math.random() * canvas.height / 0.5, 5, i / 2, 'white'));
-//     }
-//     console.log(particles);
-// }
-
-// function animate() {
-//     requestAnimationFrame(animate);
-//     c.clearRect(0, 0, canvas.width, canvas.height);
-
-//     particles.forEach(particle => {
-//         particle.update();
-
-//     });
-// }
-
-// init();
-// animate();
